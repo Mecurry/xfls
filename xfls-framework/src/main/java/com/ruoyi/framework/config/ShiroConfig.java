740a95e4a90b7f02ba8be0f6588a17c23ba7b25a
谢谢 <xmyneed@qq.com>
2020-02-04 11:34:23 +0800

    增加首页火灾分布静态页面

--- xfls-framework/src/main/java/com/ruoyi/framework/config/ShiroConfig.java
+++ xfls-framework/src/main/java/com/ruoyi/framework/config/ShiroConfig.java
@@ -247,6 +247,7 @@ public class ShiroConfig
         filterChainDefinitionMap.put("/docs/**", "anon");
         filterChainDefinitionMap.put("/fonts/**", "anon");
         filterChainDefinitionMap.put("/img/**", "anon");
+        filterChainDefinitionMap.put("/icon/**", "anon");
         filterChainDefinitionMap.put("/newstyle/**", "anon");
         filterChainDefinitionMap.put("/ajax/**", "anon");
         filterChainDefinitionMap.put("/js/**", "anon");
