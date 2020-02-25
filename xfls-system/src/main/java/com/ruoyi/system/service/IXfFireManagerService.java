7641944ab0cc5613cbd5835e0aabefbf53c0ee70
谢谢 <xmyneed@qq.com>
2020-02-25 16:39:32 +0800

    dasdada

---
+++ xfls-system/src/main/java/com/ruoyi/system/service/IXfFireManagerService.java
@@ -0,0 +1,82 @@
+package com.ruoyi.system.service;
+
+import com.ruoyi.system.domain.XfFireManager;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 【请填写功能名称】Service接口
+ * 
+ * @author yanye
+ * @date 2020-02-05
+ */
+public interface IXfFireManagerService 
+{
+    /**
+     * 查询【请填写功能名称】
+     * 
+     * @param id 【请填写功能名称】ID
+     * @return 【请填写功能名称】
+     */
+    public XfFireManager selectXfFireManagerById(String id);
+
+
+    /**
+     * 查询列表
+     * 
+     * @param xfFireManager
+     * @return
+     */
+    public List<XfFireManager> selectXfFireManagerList(XfFireManager xfFireManager);
+
+    /**
+     * 查询列表（根据日期排序_DESC降序）
+     *
+     * @param xfFireManager
+     * @return
+     */
+    public List<XfFireManager> selectXfFireManagerListOrderByDATE(XfFireManager xfFireManager);
+
+
+    /**
+     * 新增【请填写功能名称】
+     * 
+     * @param xfFireManager 【请填写功能名称】
+     * @return 结果
+     */
+    public int insertXfFireManager(XfFireManager xfFireManager);
+
+    /**
+     * 修改【请填写功能名称】
+     * 
+     * @param xfFireManager 【请填写功能名称】
+     * @return 结果
+     */
+    public int updateXfFireManager(XfFireManager xfFireManager);
+
+    /**
+     * 批量删除【请填写功能名称】
+     * 
+     * @param ids 需要删除的数据ID
+     * @return 结果
+     */
+    public int deleteXfFireManagerByIds(String ids);
+
+    /**
+     * 删除【请填写功能名称】信息
+     * 
+     * @param id 【请填写功能名称】ID
+     * @return 结果
+     */
+    public int deleteXfFireManagerById(String id);
+    public List<XfFireManager> getStatisticsList(Map<String,Object> map);
+
+    /**
+     * 根据年查询坐标数据
+     * @param fireDate
+     * @return
+     */
+    public List<XfFireManager> selectXfFireManagerListByYear(String fireDate);
+}
